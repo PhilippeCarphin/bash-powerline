a8cdf715d1037ed9b1882c23dbab94ca88dc81c8
Philippe Carphin <philippe.carphin2@canada.ca>
2023-01-13 19:01:05 -0500

    Many updates

--- powerline.sh
+++ powerline.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-if ! source ~/.git-prompt.sh ; then
+if ! source ~/.git-prompt-phil.sh ; then
     echo "${BASH_SOURCE[0]} expects ~/.git-prompt.sh to exist.  It can be obtained at 'https://github.com/git/git/blob/master/contrib/completion/git-prompt.sh'.  Ideally the one corresponding to your version of git but I always just get the one from 'master' (this link).  There is always a chance that a new one will use git commands that your version of git does not have."
     return 1
 fi
+GIT_PS1_SHOWUNTRACKEDFILES=1
+GIT_PS1_SHOWUPSTREAM=verbose
 
-__next_line=""
 __next_line_mode(){
     if [[ -z "${__next_line}" ]] ; then
         __next_line="ON"
@@ -14,12 +15,18 @@ __next_line_mode(){
     fi
 }
 
-__demo=""
+is_git_submodule(){
+    [[ -n $(git rev-parse --show-superproject-working-tree 2>/dev/null) ]]
+}
+
 __demo_mode(){
     if [[ -z "${__demo}" ]] ; then
         __demo="ON"
+        __original_ps2="${PS2}"
+        PS2=" > "
     else
         __demo=""
+        PS2="${__original_ps2}"
     fi
 }
 
@@ -88,7 +95,7 @@ __number_to_foreground_code(){
 __prompt_section(){
     local content=$1
     local bg_section=$2
-    local fg_section=$4
+    local fg_section=$3
 
     local fg_code=$(__number_to_foreground_code ${fg_section})
     local bg_code=$(__number_to_background_code ${bg_section})
@@ -112,20 +119,32 @@ __git_pwd() {
     printf "\[\033[1;4m\]${outer}\[\033[22;24m\]${inner:+/${inner}}"
 }
 
+__powerline_job_colors(){
+    c_host_bg=52 #90
+    c_jobid=88 #127
+    # c_user=124 # 164
+    # c_dir=160 # 170
+}
+
+
 __prompt(){
     local previous_exit_code=${1}
 
     local c_host_bg=27
     local c_host_fg=
+    local c_jobid=130
     local c_user=33
     local c_dir=74
-    local c_dir_fg=
-    local c_git_headless=52
+    local c_dir_fg=15
+    local c_git_headless=88
     local c_git_dirty=184
     local c_git_clean=2
     local c_exit_code_success=34
     local c_exit_code_failure=9
     local c_next_line=27
+    if [[ -n ${PBS_JOBID} ]] ; then
+        __powerline_job_colors
+    fi
 
     #
     # Exit code section, followed by host section
@@ -136,31 +155,44 @@ __prompt(){
     else
         c_exit_code="${c_exit_code_failure}"
     fi
-    __prompt_section " ${previous_exit_code} " "${c_exit_code}" "${c_host_bg}" "0"
+    __prompt_section " ${previous_exit_code} " "${c_exit_code}" "0"
     __prompt_triangle "${c_exit_code}" "${c_host_bg}"
 
     #
     # Host section followed by user section
     #
-    __prompt_section "\\h" "${c_host_bg}" "${c_user}" "${c_host_fg}"
-    __prompt_triangle "${c_host_bg}" "${c_user}"
+    __prompt_section "\\h" "${c_host_bg}" "${c_host_fg}"
+    if [[ -n ${PBS_JOBID} ]] ; then
+        __prompt_triangle "${c_host_bg}" "${c_jobid}"
+        __prompt_section "${PBS_JOBID}" "${c_jobid}"
+
+        __prompt_triangle "${c_jobid}" "${c_user}"
+    else
+        __prompt_triangle "${c_host_bg}" "${c_user}"
+    fi
 
     #
     # Host section followed by directory section
     #
-    __prompt_section "\\u" "${c_user}" "${c_dir}"
+    __prompt_section "\\u" "${c_user}"
     __prompt_triangle "${c_user}" "${c_dir}"
 
-    local g
+
     local info
-    local git_color
-    # PS1=""
-    # __git_ps1 "." "." "(%s asdfasd)"
-    # echo "PS1 after __git_ps1 = ${PS1}"
     if info="$(git rev-parse --git-dir 2>/dev/null)" ; then
+        # Use single-argument form of __git_ps1 to get the text of the
+        # git part of the prompt.
+        local git_part
+        if is_git_submodule ; then
+            git_part="$(__git_ps1 " %s \033[1;4mSM\033[21;24m ")"
+        else
+            git_part="$(__git_ps1 " %s")"
+        fi
+        # Copy somt code from git-prompt.sh to determine what color to use
+        # for the git part of the prompt.
+        local git_color
         local g="${info%$'\n'}"
         local b="$(git symbolic-ref HEAD 2>/dev/null)"
-        local git_part="$(__git_ps1 "(%s)")"
         local head
         __git_eread "$g/HEAD" head
         b="${head#ref: }"
@@ -171,41 +203,132 @@ __prompt(){
             git_color="${c_git_clean}"
         else
             git_color="${c_git_dirty}"
+            git_part="${git_part} $(git_time_since_last_commit)"
         fi
+
         #
         # Directory section followed by git section
         #
-        __prompt_section "$(__git_pwd)" "${c_dir}" "${git_color}" "${c_dir_fg}"
+        __prompt_section "$(__git_pwd)" "${c_dir}" "${c_dir_fg}"
         __prompt_triangle "${c_dir}" "${git_color}"
 
         #
         # Git section followed by nothing
         #
-        __prompt_section "${git_part}" "${git_color}" "" "0"
+        __prompt_section "${git_part}" "${git_color}" "0"
         __prompt_triangle "${git_color}" ""
     else
         #
         # Directory section followed by nothing
         #
-        __prompt_section "\\w" "${c_dir}" "" "${c_dir_fg}"
+        __prompt_section "\\w" "${c_dir}" "${c_dir_fg}"
         __prompt_triangle "${c_dir}" ""
     fi
     if [[ -n ${__next_line} ]] ; then
         printf "%s" "\n"
-        __prompt_section "$" "${c_next_line}" "" ""
-        __prompt_triangle "${c_host_bg}" ""
+        __prompt_section "$" "${c_next_line}" 15
+        __prompt_triangle "${c_next_line}" ""
     fi
 }
 
 __set_ps1(){
     local previous_exit_code=$?
+
+    local user_had_xtrace
+    if shopt -op xtrace >/dev/null; then
+        user_had_xtrace=true
+        printf "Disabling xtrace during prompt evaluation\n"
+    else
+        user_had_xtrace=false
+    fi
+    __phil_ps1_deal_with_vscode
+
+    set +o xtrace
     if [[ -n "${__demo}" ]] ; then
-        PS1='\n $ '
+        PS1="=> ${previous_exit_code}\n\n $ "
     else
         PS1="$(__prompt ${previous_exit_code}) "
     fi
+
+    if [[ "${user_had_xtrace}" == true ]] ; then
+        printf "Reenabling xtrace after prompt evaluation\n"
+        set -x
+    fi
 }
-PS2="$(__prompt_section ">" "5" "" ""; __prompt_triangle "5" "") "
+
+# Cool PS2 that goes up and changes the last bit of PS1 in Next line mode
+PS2="\[\r\033[A\033[1;105m\]>\[\033[35;49m\]\[\033[B\r\\033[105;39m\]>\[\033[35;49m\]\[\033[0m\] "
+# - beginning of line
+# - up one line (\033[A)
+# - ovewrite white on blue $ followed by blue '' on default bg by the same thing but with purple instead of blue
+# - go down one line (\033[B)
+# - beginning of line \r
+# - Write a white '>' on purple bg followed by a purple '' on default bg.
+# Only works in "next_line_mode"
+# PS2="$(__prompt_section ">" "5" "15" ""; __prompt_triangle "5" "") "
 
 PROMPT_COMMAND=__set_ps1
+
+###############################################################################
+# At some point, inside VSCode shell, the displayed exit code was always 1
+# regardless of whether or not the previous command had succeeded or not.
+#
+# VSCode launches integrated terminals with
+#
+#   /usr/bin/bash --init-file .../vscode-server/.../shellIntegration-bash.sh
+#
+# which
+# - loads either the ~/.bashrc or one of ~/.bash_profile, ~/.bash_login, ~/.profile
+#   based on VSCODE_SHELL_LOGIN (instead of passing -l to the command becasue it is
+#   incompatible with --init-file)
+#
+# __vsc_prompt_command_original which stores the status of the previous command
+# in __vsc_status and then does stuff to let VSCode know whether the command
+# succeeded or failed and then calls what I had set as the PROMPT_COMMAND.
+# And by the time __my_git_ps1 function is called $? is always 1!
+################################################################################
+__phil_ps1_deal_with_vscode(){
+    if [[ -n ${__vsc_status-} ]] ; then
+        previous_exit_code=${__vsc_status}
+    fi
+}
+
+git_time_since_last_commit() {
+    # This checks if we are in a repo an that there is a commit
+    local repo_info
+    repo_info=$(git rev-parse --is-inside-work-tree 2>/dev/null)
+    if [ -z "$repo_info" ] ; then
+        return
+    fi
+
+    local last_commit_unix_timestamp now_unix_timestamp seconds_since_last_commit
+    if ! last_commit_unix_timestamp=$(git log --pretty=format:'%at' -1 2> /dev/null) ; then
+        return
+    fi
+    now_unix_timestamp=$(date +%s)
+    seconds_since_last_commit=$(($now_unix_timestamp - $last_commit_unix_timestamp))
+
+    format_seconds $seconds_since_last_commit
+}
+
+format_seconds(){
+	seconds=$1
+    # Totals
+    MINUTES=$(($seconds / 60))
+    HOURS=$(($seconds /3600))
+    # Sub-hours and sub-minutes
+    seconds_per_day=$((60*60*24))
+    DAYS=$(($seconds / $seconds_per_day))
+    SUB_HOURS=$(( $HOURS % 24))
+    SUB_MINUTES=$(( $MINUTES % 60))
+    if [ "$DAYS" -gt 5 ] ; then
+        echo "${DAYS}days"
+    elif [ "$DAYS" -gt 1 ]; then
+        echo "${DAYS}d${SUB_HOURS}h${SUB_MINUTES}m"
+    elif [ "$HOURS" -gt 0 ]; then
+        echo "${HOURS}h${SUB_MINUTES}m"
+    else
+        echo "${MINUTES}m"
+    fi
+}
 
